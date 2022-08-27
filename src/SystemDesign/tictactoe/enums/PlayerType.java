b654abc0fef5e3806260f144a1645004f0b12827
Aftab Alam <aftaba@sigmoidanalytics.com>
2022-08-27 18:35:40 +0530

    Added new features

---
+++ src/SystemDesign/tictactoe/enums/PlayerType.java
@@ -0,0 +1,6 @@
+package SystemDesign.tictactoe.enums;
+
+public enum PlayerType {
+    
+    HUMAN,COMPUTER
+}
---
+++ src/SystemDesign/tictactoe/enums/Symbol.java
@@ -0,0 +1,6 @@
+package SystemDesign.tictactoe.enums;
+
+public enum Symbol {
+    
+    X,O
+}
