d85d93db364f27f86bb62205a54f403339cdefde
Aftab Alam <aftaba@sigmoidanalytics.com>
2022-09-01 19:11:19 +0530

    Added new chess game design

--- src/SystemDesign/chessboard/entities/boardentities/Board.java
+++ src/SystemDesign/chessboard/entities/boardentities/Board.java
@@ -3,13 +3,14 @@ package SystemDesign.chessboard.entities.boardentities;
 import SystemDesign.chessboard.Position;
 import SystemDesign.chessboard.entities.ChessEntity;
 
+import java.nio.channels.Pipe;
 import java.util.Map;
 
 public class Board {
     
     Cell board[][];
     
-    public void updateBoard(Position position){
+    public void updateBoard(Position startPosition, Position endPosition, ChessEntity chessEntity){
         
     }
     
